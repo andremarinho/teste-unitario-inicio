2dfac135ca9e0c903014904151381ff62027f8c0
andremarinho <andre.bmarinho@hotmail.com>
2021-02-09 13:14:40 -0300

    Implementacao primeiro teste

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -29,6 +30,35 @@ public class LocacaoService {
 	}
 
 	public static void main(String[] args) {
+		//F - First
+		//I - Indepedent
+		//R - Repeatable
+		//S -  Self verification
+		//T - Timely
+		
+		
+		//Cenario
+		Usuario usuario = new Usuario();
+		Filme filme = new Filme();
+		LocacaoService locacaoService = new LocacaoService();
+		
+		usuario.setNome("José da Silva");
+		filme.setNome("E o vento levou");
+		filme.setPrecoLocacao(1.0);
+		filme.setEstoque(10);
+		
+		//Acao
+		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+		
+		
+		//Verificacao
+		if(locacao != null){
+			System.out.println(locacao.getValor()>0);
+			System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+			
+		}else{
+			System.out.println("Não funcionou!!!");
+		}
 		
 	}
 }
